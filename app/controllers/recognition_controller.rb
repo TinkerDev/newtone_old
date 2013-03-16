a6c2d154d844a0de6fbd99b3a479176dcf122163
Alexander Logunov <unlovedru@gmail.com>
2013-03-17 02:18:17 +0400

    puts remove

--- app/controllers/recognition_controller.rb
+++ app/controllers/recognition_controller.rb
@@ -1,7 +1,6 @@
 class RecognitionController < ApplicationController
   def recognize
     audio = params[:sample][:upload].tempfile.path
-    puts audio
     recognition = Recognition.new audio
     render :json => recognition.friendly_recognize
   end
