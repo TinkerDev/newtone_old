34a184b6eb173dad7588ef7dea69534e6bbd55f1
Alexander Logunov <unlovedru@gmail.com>
2013-03-17 02:12:54 +0400

    select instead of upload

--- app/controllers/recognition_controller.rb
+++ app/controllers/recognition_controller.rb
@@ -1,6 +1,7 @@
 class RecognitionController < ApplicationController
   def recognize
     audio = params[:sample][:upload]
+
     recognition = Recognition.new audio
     render :json => recognition.friendly_recognize
   end
