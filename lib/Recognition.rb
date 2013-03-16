30e124703c209c586fe9a90ffab14fd81117af02
Alexander Logunov <unlovedru@gmail.com>
2013-03-17 00:06:08 +0400

    wow, we have fingerprint now

--- lib/Recognition.rb
+++ lib/Recognition.rb
@@ -1,11 +1,17 @@
 class Recognition
-  attr_accessor :audio
+  attr_reader :audio_file_path, :raw_fingerprint, :raw_code
 
-  def initialize audio
-    @audio = audio
+  def initialize audio_file_path
+    @audio_file_path = audio_file_path
+  end
+
+  def make_raw_fingerprint
+    @raw_fingerprint = Newtone::Fingerprint.build audio_file_path
+    @raw_code = @raw_fingerprint['code']
   end
 
   def recognize
-    return {:artist => 'Michael Jackson', :track => 'Billy Jean'}
+    self.make_raw_fingerprint
+    return {:artist => 'Michael Jackson', :track => 'Billy Jean', :status => 1}
   end
 end
