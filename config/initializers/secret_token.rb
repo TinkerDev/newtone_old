4efa9a13bd70a6d361fa8d1437e4b25a2324e279
Alexander Logunov <unlovedru@gmail.com>
2013-03-16 12:00:17 +0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newtone::Application.config.secret_token = '3d38523878604589a8e20af8bfe344a56cf510ecafb1cb293d988ce20ac5ee13a6ed79c8924036c213a062079e7c13463f1f740639b9cd733aac5f4bddf6769c'
