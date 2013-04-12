0108134f8ed25df7b2ef15a46757081cbf5f9783
Alexander Logunov <unlovedru@gmail.com>
2013-04-12 21:42:52 +0400

    parser update

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,28 +1,4 @@
 class Parser
-  def self.test
-
-    #s1 = ''
-    #s2 = ''
-
-    #app = VK::Application.new access_token: VkToken.get_token
-    #app.audio.search :q=>'gaga'
-
-
-
-
-    #session = ::VkApi::Session.new s1, s2
-    #require 'open-uri'
-    #x =
-    #x = 'http://vk.com'
-    #x ='https://oauth.vk.com/access_token?client_id=' + s1 + '&client_secret=' + s2 + '&grant_type=client_credentials'
-    #token = JSON.parse(open(x).read)['access_token']
-    #session.audio.search :q => 'gagga', :access_token => token
-    #session.get_user_settings
-    #x = 'https://api.vk.com/method/searchUsers?q=Gaga&access_token='+token
-    #JSON.parse(open(x).read)
-    #open(x).read
-
-  end
 
   def self.merge_artists_from_lastfm_chart_top_artist
     artists = LastfmLibrary.get_chart_top_artists
