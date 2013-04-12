bbb8c45cd0b7065e2b6f7da877116af3afe48f67
Alexander Logunov <unlovedru@gmail.com>
2013-04-12 22:45:54 +0400

    unique track

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,5 @@
 class Artist < ActiveRecord::Base
   attr_accessible :name, :tracks_count
   has_many :tracks
+  validates :name, :presence => true
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -5,13 +5,12 @@ class Track < ActiveRecord::Base
                   :title, :release, :genre, :duration, :version, :artist_name
   mount_uploader :track_file, TrackUploader
 
-  validates :track_file, :presence => true, :if => Proc.new { |t| t.raw_fingerprint.blank? }, :on => :create
-  validates :raw_fingerprint, :presence => true, :if => Proc.new { |t| t.track_file.blank? }, :on => :create
-  validates :artist, :presence => true, :if => Proc.new { |t| t.artist_name.blank? }, :on => :create
-  validates :artist_name, :presence => true, :if => Proc.new { |t| t.artist.nil? }, :on => :create
+  validates :fingerprint, :presence => true, :on => :create
+  validates :artist, :title, :presence => true
+  validates :artist_id, :uniqueness => {:scope => :title}
 
-  before_create :generate_fingerprint
-  before_create :copy_fingerprint_info
+  before_validation :generate_fingerprint
+  before_validation :copy_fingerprint_info
   after_save :update_solr
   after_create :remove_track_file!
   after_create :delete_track_file_folder
@@ -21,19 +20,17 @@ class Track < ActiveRecord::Base
   belongs_to :artist, :counter_cache => true
 
   def generate_fingerprint
-    if track_file.present?
-      @fingerprint = Fingerprint.new track_file.file.file
-    else
-      @fingerprint = Fingerprint.new raw_fingerprint
-    end
+    @fingerprint = Fingerprint.new track_file.file.file if track_file.present?
+    @fingerprint = Fingerprint.new raw_fingerprint if raw_fingerprint.present?
   end
 
   def copy_fingerprint_info
-    self.artist_name = @fingerprint.artist
+    self.artist_name = @fingerprint.artist if artist_name.blank?
     if self.artist.nil?
       artist = Artist.find_or_create_by_name(self.artist_name)
       self.artist = artist
     end
+
     attributes=[:title, :release, :genre, :duration, :version].inject({}){|res, attr| val = @fingerprint.send(attr); res[attr] = val unless val.blank?; res;}
     attributes[:track_id] = Digest::MD5.hexdigest(Time.now.to_s(:db)+rand(100000).to_s)
     self.assign_attributes(attributes)
